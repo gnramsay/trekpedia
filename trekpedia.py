d8f0407f55936e5ca6140bc1cd2f093ef2071338
Grant Ramsay <grant@gnramsay.com>
2022-06-12 19:25:51 +0100

    wrap main functionality  in a runner function
    Signed-off-by: Grant Ramsay <grant@gnramsay.com>

--- trekpedia.py
+++ trekpedia.py
@@ -4,6 +4,7 @@ Produce JSON dumps of Star Trek data suitable for adding to an API.
 """
 import json
 import re
+import sys
 
 import requests
 from bs4 import BeautifulSoup
@@ -312,20 +313,42 @@ class Trekpedia:
 # ---------------------------------------------------------------------------- #
 #                                   Main Code                                  #
 # ---------------------------------------------------------------------------- #
-trekpedia = Trekpedia()
+def main(_args):
+    """Run the main program, parse and save data from Wikipedia."""
+    trekpedia = Trekpedia()
 
-print(
-    "Trekpedia : Parse 'Star Trek' data from the Web and save as JSON.\n"
-    "(C)2022 Grant Ramsay (grant@gnramsay.com)\n"
-    f"Version {trekpedia.version}\n"
-)
+    print(
+        "Trekpedia : Parse 'Star Trek' data from the Web and save as JSON.\n"
+        "(C)2022 Grant Ramsay (grant@gnramsay.com)\n"
+        f"Version {trekpedia.version}\n"
+    )
 
-# -------- get the series info and save to a JSON file for later use. -------- #
-print("Getting Series Data...", end="")
-trekpedia.get_series_info()
-trekpedia.save_json("output/star_trek_series_info.json", trekpedia.series_data)
-print(" Done!\n")
+    # ------ get the series info and save to a JSON file for later use. ------ #
+    print("Getting Series Data...", end="")
+    trekpedia.get_series_info()
+    trekpedia.save_json(
+        "output/star_trek_series_info.json", trekpedia.series_data
+    )
+    print(" Done!\n")
 
-# --------------- loop through each series and parse then save --------------- #
-for series_data in trekpedia.series_data.items():
-    trekpedia.parse_series(series_data)
+    # ------------- loop through each series and parse then save ------------- #
+    for series_data in trekpedia.series_data.items():
+        trekpedia.parse_series(series_data)
+
+
+# ---------------------------------------------------------------------------- #
+#                         Wrapper to run the main code                         #
+# ---------------------------------------------------------------------------- #
+def run():
+    """Call :func:`main` passing any CLI arguments."""
+    try:
+        main(sys.argv[1:])
+    except KeyboardInterrupt:
+        pass
+
+
+# ---------------------------------------------------------------------------- #
+#              Actually run our code, unless we have been imported             #
+# ---------------------------------------------------------------------------- #
+if __name__ == "__main__":
+    run()
