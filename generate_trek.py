5764d753da8f6741138dc78a5c0d9a5c0964a7b9
Grant Ramsay <grant@gnramsay.com>
2023-09-03 16:53:15 +0100

    migrate console output to use 'rich'
    Signed-off-by: Grant Ramsay <grant@gnramsay.com>

--- generate_trek.py
+++ generate_trek.py
@@ -5,7 +5,7 @@ Produce JSON dumps of Star Trek data suitable for adding to an API.
 import sys
 
 import colorama
-from blessings import Terminal
+from rich import print  # pylint: disable=redefined-builtin
 
 from lib.trekpedia import Trekpedia
 
@@ -22,31 +22,29 @@ def main(_args):
         trekpedia = Trekpedia(summary_url=MAIN_URL, json_template=JSON_TEMPLATE)
 
         colorama.init()
-        t = Terminal()  # pylint: disable=invalid-name
 
         print(
-            f"Trekpedia : Parse '{t.cyan}Star Trek{t.normal}' "
+            "Trekpedia : Parse '[cyan]Star Trek[/cyan]' "
             "data from the Web and save as JSON.\n"
         )
         print("\u00a9 2023 Grant Ramsay <grant@gnramsay.com>\n")
         print(f"Version {trekpedia.version}\n")
 
         # ---- get the series info and save to a JSON file for later use. ---- #
-        print("Getting Series Data...", end="")
+        print("Getting Series Data ... ", end="")
         trekpedia.get_series_info()
         trekpedia.save_json(
             "output/star_trek_series_info.json", trekpedia.series_data
         )
-        print(" Done!\n")
+        print("Done!\n")
 
         # ----------- loop through each series and parse then save ----------- #
         for series_data in trekpedia.series_data.items():
             if series_data[0] not in [11]:
                 trekpedia.parse_series(series_data)
     except KeyboardInterrupt:
-        t = Terminal()  # pylint: disable=invalid-name
         print("\r", " " * 80)
-        print(f"{t.red}{t.bold}Escape Pressed, processing ABORTED.{t.normal}\n")
+        print("[red][bold]Escape Pressed, processing ABORTED.\n")
 
 
 # ---------------------------------------------------------------------------- #
